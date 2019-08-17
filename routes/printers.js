4892cbcdbed7a50c55653899a6f39b8d090c9989
shittt <lspssapple@gmail.com>
2019-08-17 19:50:04 +0800

    b2

--- routes/printers.js
+++ routes/printers.js
@@ -4,19 +4,18 @@ var router = express.Router();
 var printer = require("printer");
 var filename = 'merge_1.png'
 router.get('/', function(req, res, next) {
-    res.render('printer', { title: 'Printers', printername: printer.getDefaultPrinterName(), printersupformat:printer.getSupportedPrintFormats(printer.getDefaultPrinterName()),printerpapersize:printer.getSelectedPaperSize(printer.getDefaultPrinterName()),printerjobcommand:printer.getSupportedJobCommands(),prninterdriveroptons:printer.getPrinterDriverOptions(printer.getDefaultPrinterName())});
+    res.render('printer', { title: 'Printers', printername: printer.getDefaultPrinterName(), printersupformat:printer.getSupportedPrintFormats(printer.getDefaultPrinterName()),printerpapersize:"Not Support",printerjobcommand:printer.getSupportedJobCommands(),prninterdriveroptons:"Not Support"});
     
 
   });
-router.post('/printpic', function(req, res, next) {
+router.get('/printpic', function(req, res, next) {
     console.log('platform:', process.platform);
     console.log('try to print file: ' + filename);
-    console.log(printer.getPrinterDriverOptions(printer.getDefaultPrinterName()));
     if( process.platform != 'win32') {
-      printer.printFile({filename:req.body.filename,
-        printer: process.env[3], 
-        type: 'PNG' ,
-        options: {PageSize:'P6x4'},
+      printer.printDirect({filename:req.body.filename,
+        printer:  printer.getDefaultPrinterName(),
+        type: 'RAW' ,
+        //options: {PageSize:'P6x4'},
         success:function(jobID){
           console.log("sent to printer with ID: "+jobID);
           res.send('Printed <a href="../printers" title="Back">Back</a>');
